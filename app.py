88fae51354a13ad51e6cf252a569efdb1d56e2d0
iamray1013 <raymondangel.irudayasagayakuma@dcmail.ca>
2024-07-28 23:32:42 -0400

    update

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def predict():
     return render_template('index.html', prediction_text=f'Predicted Fish Weight: {predicted_weight:.2f} grams')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
